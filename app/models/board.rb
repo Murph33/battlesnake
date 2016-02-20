62772b46239803bfee300111868f016a3a40b63e
Brendan Deere <brendan.g.deere@gmail.com>
2016-02-20 13:58:20 -0800

    Board uses Game Object
    also initializes sub objects

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,24 +1,27 @@
-class Board
+class Board < GameObject
   attr_accessor :width, :height, :snakes, :food, :walls, :gold
-
-  def initialize(width: nil, height: nil, snakes: nil, food: nil, walls: nil, gold: nil)
-    @width, @height, @snakes, @food, @walls, @gold =
-      width, height, snakes, food, walls, gold
-  end
-
-  def self.create(attributes = {})
-    self.new.assign_attributes attributes
-  end
-
-  def assign_attributes(attributes = {})
-    self.tap do |board|
-      attributes.each { |key, value| self.send "#{ key }=", value }
-    end
-  end
 
   def snakes=(snake_data)
     snake_data.map do |snake_attributes|
       Snake.new snake_attributes
+    end
+  end
+
+  def food=(food_data)
+    food_data.map do |food_attributes|
+      FoodItem.new food_attributes
+    end
+  end
+
+  def walls=(wall_data)
+    wall_data.map do |wall_attributes|
+      Wall.new wall_attributes
+    end
+  end
+
+  def golds=(gold_data)
+    gold_data.map do |gold_attributes|
+      GoldPiece.new gold_attributes
     end
   end
 end
