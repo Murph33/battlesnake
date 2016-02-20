39cb2f4eae2c788b42cccb3fc3d1438fec8a2292
Brendan Deere <brendan.g.deere@gmail.com>
2016-02-20 15:05:43 -0800

    wip

--- app/controllers/snakes_controller.rb
+++ app/controllers/snakes_controller.rb
@@ -3,15 +3,19 @@ class SnakesController < ApplicationController
   def setup
     response_object = {
       color: "#fff000"
+      head: 'https://www.placecage.com/20/20'
     }
 
     render json: response_object
   end
 
   def start
+    board = Board.new params.keep_if { |k, v| board_params.include? k }
     response_object = {
       taunt: "BONESAW IS READY!!"
     }
+
+
 
     render json: response_object
   end
@@ -26,5 +30,11 @@ class SnakesController < ApplicationController
       taunt: "we're doing it"
     }
     render json: response_object
+  end
+
+  private
+
+  def board_params
+    [:width, :height, :snakes, :food, :walls, :gold]
   end
 end
