4657d9957ee7a4d87897cf5333967102e29eded9
Brendan Deere <brendan.g.deere@gmail.com>
2016-02-20 13:46:17 -0800

    Mass assignment for Board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,18 @@
 class Board
-  attr_reader :width, :height, :snakes, :food, :walls, :gold
+  attr_accessor :width, :height, :snakes, :food, :walls, :gold
 
   def initialize(width: nil, height: nil, snakes: nil, food: nil, walls: nil, gold: nil)
     @width, @height, @snakes, @food, @walls, @gold =
       width, height, snakes, food, walls, gold
+  end
+
+  def self.create(attributes = {})
+    self.new.assign_attributes attributes
+  end
+
+  def assign_attributes(attributes = {})
+    self.tap do |board|
+      attributes.each { |key, value| self.send "#{ key }=", value }
+    end
   end
 end
