b51ac1011a106e662a080138c919317eb011062a
Murphy Stroppa <murph33@gmail.com>
2016-02-20 18:02:22 -0800

    go west young man

--- app/controllers/snakes_controller.rb
+++ app/controllers/snakes_controller.rb
@@ -18,15 +18,19 @@ class SnakesController < ApplicationController
   end
 
   def move
-    board = Board.new params.keep_if { |k, v| board_params.include? k }
-    snake = params{snake}
-    our_snake_hash = params{snakes}.find { |snake| snake[:id] == Snake::SNAKE_ID }
-    snake = Snake.new(our_snake_hash[:coords][0], our_snake_hash[:coords][1..-1])
+    # binding.pry
+    # board = Board.new params.keep_if { |k, v| board_params.include? k }
+    # # board = Board.new params
+    #
+    # our_snake_hash = params["snakes"].find { |snake| snake[:id] == Snake::SNAKE_ID }
+    # 
+    # snake = Snake.new(our_snake_hash[:coords][0], our_snake_hash[:coords][1..-1])
+
     # our_snake = board.our_snake
     # move = our_snake.want_to_move(board)
 
     response_object = {
-      move: move,
+      move: "west",
       taunt: "we're doing it"
     }
     render json: response_object
@@ -35,6 +39,6 @@ class SnakesController < ApplicationController
   private
 
   def board_params
-    [:width, :height, :snakes, :food, :walls, :gold]
+    %w(width height snakes food walls gold)
   end
 end
